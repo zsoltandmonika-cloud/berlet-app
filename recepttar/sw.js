110ce4c386aadefd2e9bc6d8d106b9c61f5421a8
zsoltandmonika-cloud <zsoltandmonika@gmail.com>
2026-09-18 18:48:46 +0200

    Add local admin editing to Lena Recepttar 2.2

--- recepttar/sw.js
+++ recepttar/sw.js
@@ -1,6 +1,5 @@
-
-const C="lena-recepttar-21-shell";
-const SHELL=["./","./index.html","./styles.css","./app.js","./data.js","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
+const C="lena-recepttar-22-shell";
+const SHELL=["./","./index.html","./styles.css?v=22","./app.js?v=22","./data.js?v=22","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
-self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
-self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{if(x.ok){const y=x.clone();caches.open(C).then(c=>c.put(e.request,y))}return x})))});
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(fetch(e.request).then(r=>{if(r.ok){const c=r.clone();caches.open(C).then(x=>x.put(e.request,c))}return r}).catch(()=>caches.match(e.request)))});
