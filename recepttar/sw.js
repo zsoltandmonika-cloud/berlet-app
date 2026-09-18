30536ff65c9d19280c37dcf028121ddac8ff05d2
zsoltandmonika-cloud <zsoltandmonika@gmail.com>
2026-09-19 01:20:50 +0200

    Refresh Studio shell cache v44

--- recepttar/sw.js
+++ recepttar/sw.js
@@ -1,5 +1,5 @@
-const C="lena-recepttar-studio-43-shell";
-const SHELL=["./","./index.html","./styles.css?v=28","./app.js?v=36","./data.js?v=24","./readable-data.js?v=34","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
+const C="lena-recepttar-studio-44-shell";
+const SHELL=["./","./index.html","./styles.css?v=28","./app.js?v=37","./data.js?v=24","./readable-data.js?v=34","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(fetch(e.request).then(r=>{if(r.ok){const c=r.clone();caches.open(C).then(x=>x.put(e.request,c))}return r}).catch(()=>caches.match(e.request)))})
